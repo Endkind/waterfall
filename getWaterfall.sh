42ff9cc6b47f0e04fb3799666c8561a37d8a47c3
Endkind <endkind.ender@gmail.com>
2025-01-25 05:43:39 +0100

    - fix `docker-entrypoint.sh` - fix `getWaterfall.sh`

--- getWaterfall.sh
+++ getWaterfall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 VERSION=$(cat /endkind/waterfall_version)
 BASE_URL="https://api.papermc.io/v2/projects/waterfall"
 
@@ -9,6 +9,7 @@ fi
 LATEST_BUILD=$(curl -s "$BASE_URL/versions/$VERSION" | jq -r '.builds | .[-1]')
 
 curl -o "/endkind/server.jar" -L "$BASE_URL/versions/$VERSION/builds/$LATEST_BUILD/downloads/waterfall-$VERSION-$LATEST_BUILD.jar"
+
 if [ $? -eq 0 ]; then
     echo "Download Waterfall Version ($VERSION) Build ($LATEST_BUILD)"
 else
